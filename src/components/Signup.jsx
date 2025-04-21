23037918257a655390ce5007f76f8e591e390d91
WodaMineralna <raknieborak65@gmail.com>
2025-04-21 12:21:24 +0200

    Managing form-dependent state with useActionState()

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,3 +1,5 @@
+import { useActionState } from "react";
+
 import {
   isEmail,
   isNotEmpty,
@@ -18,7 +20,7 @@ const ERROR_MESSAGES = {
 };
 
 export default function Signup() {
-  function signupAction(formData) {
+  function signupAction(prevFormState, formData) {
     const email = formData.get("email");
     const password = formData.get("password");
     const confirmPassword = formData.get("confirm-password");
@@ -42,10 +44,20 @@ export default function Signup() {
     if (!terms) errors.push(ERROR_MESSAGES.terms);
 
     console.log(errors); // testing
+
+    if (errors.length > 0) {
+      return { errors };
+    }
+
+    return { errors: null };
   }
 
+  const [formState, formAction, pending] = useActionState(signupAction, {
+    errors: null,
+  });
+
   return (
-    <form action={signupAction}>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -129,6 +141,14 @@ export default function Signup() {
           agree to the terms and conditions
         </label>
       </div>
+
+      {formState.errors && (
+        <ul className="error">
+          {formState.errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
 
       <p className="form-actions">
         <button type="reset" className="button button-flat">
